276fe202b0d0eaeef369c54d4de3cfe87318ad40
daehan.lawplatform <daehan@lawplatform.kr>
2023-08-10 17:08:58 +0900

    feat: add gui button and interectoin event

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 			<div className="fixed left-20 top-20">
 
 
-				<div className="card w-96 bg-base-100 shadow-xl ">
+				<div className="card w-96 bg-base-100 shadow-xl " id="D">
 					<figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
 					<div className="card-body">
 						<h2 className="card-title">Shoes!</h2>
